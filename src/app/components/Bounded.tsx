85493f0e53b41672e5509ed510954978cf974957
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-06-06 02:38:47 +1200

    add: vertical padding for bounded layout

--- src/app/components/Bounded.tsx
+++ src/app/components/Bounded.tsx
@@ -7,7 +7,7 @@ type BoundedProps = {
 
 export default function Bounded({ children, className = "" }: BoundedProps) {
   return (
-    <div className={`w-full max-w-[1920px] mx-auto px-6 md:px-20 ${className}`}>
+    <div className={`w-full max-w-[1920px] mx-auto px-6 md:px-20 pt-38 pb-32 ${className}`}>
       {children}
     </div>
   );
