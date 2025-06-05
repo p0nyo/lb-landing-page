af5e22ee51fa9930a47cb0a70ff2dce5631a9859
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-06-06 02:27:11 +1200

    fix: header button props wrong name

--- src/app/components/Header/HeaderButton.tsx
+++ src/app/components/Header/HeaderButton.tsx
@@ -1,10 +1,10 @@
-type JoinNowButtonProps = {
+type HeaderButtonProps = {
   text: string;
   bgColor?: string;
   textColor?: string;
 };
 
-export default function HeaderButton({ text, bgColor = "bg-lb-red", textColor = "text-white"}: JoinNowButtonProps) {
+export default function HeaderButton({ text, bgColor = "bg-lb-red", textColor = "text-white"}: HeaderButtonProps) {
   return (
     <button className={`w-auto h-10 py-2 px-6 ${bgColor} ${textColor} rounded-md`}>
       {text}
