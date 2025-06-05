9de5648f571cb5452dee0bccf73956d7e289ba49
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-06-06 01:16:12 +1200

    edit: background color to white

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-100 py-6">
+    <footer className="w-full bg-white py-6">
       <div className="max-w-7xl mx-auto px-4 flex">
         {/* Logo */}
         <div className="flex-1 flex justify-start">
