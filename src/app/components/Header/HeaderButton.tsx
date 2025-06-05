177eeed438886af10958a02880bb8ace69140cdb
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-06-06 01:02:31 +1200

    prettier formatting

--- src/app/components/Header/HeaderButton.tsx
+++ src/app/components/Header/HeaderButton.tsx
@@ -6,7 +6,7 @@ type JoinNowButtonProps = {
 
 export default function HeaderButton({ text, bgColor = "bg-lb-red", textColor = "text-white"}: JoinNowButtonProps) {
   return (
-    <button className={`w-auto h-10 py-2 px-6 ${bgColor} ${textColor} rounded-sm`}>
+    <button className={`w-auto h-10 py-2 px-6 ${bgColor} ${textColor} rounded-md`}>
       {text}
     </button>
   );
