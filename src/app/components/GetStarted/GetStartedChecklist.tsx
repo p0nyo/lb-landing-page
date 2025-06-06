cdaeb80b010db9f9555753fd4fa5856856ee0b5f
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-06-06 15:22:56 +1200

    add: get started section

---
+++ src/app/components/GetStarted/GetStartedChecklist.tsx
@@ -0,0 +1,20 @@
+import { TickIcon } from "@/app/components/common";
+
+type GetStartedChecklistProps = {
+  checklistItems: string[];
+};
+
+export default function GetStartedChecklist({ checklistItems }: GetStartedChecklistProps) {
+  return (
+    <div className="flex-1 bg-lb-grey px-10 py-12 rounded-lg">
+      <ul className="space-y-4 text-lb-red">
+        {checklistItems.map((item, index) => (
+          <li key={index} className="flex flex-col items-start">
+            <TickIcon />
+            <span className="text-black font-medium">{item}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
