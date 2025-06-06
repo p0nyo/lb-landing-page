53c959b4f1bcce3e6bb044498b5cf02d68038a1e
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-06-06 14:57:56 +1200

    add: download call to action section + icons

--- src/app/components/common/Bounded.tsx
+++ src/app/components/common/Bounded.tsx
@@ -7,7 +7,7 @@ type BoundedProps = {
 
 export default function Bounded({ children, className = "" }: BoundedProps) {
   return (
-    <div className={`w-full max-w-7xl mx-auto px-6 md:px-20 py-20 md:py-32 ${className}`}>
+    <div className={`w-full max-w-7xl mx-auto px-6 md:px-20 py-20 md:py-26 ${className}`}>
       {children}
     </div>
   );
